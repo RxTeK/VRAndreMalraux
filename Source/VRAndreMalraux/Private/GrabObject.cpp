e7065fce4a1c827c86ba1f1f10e107ec5c351039
Morgan50 <115717766+Morgan5000001@users.noreply.github.com>
2025-04-10 17:02:00 +0200

    Compil c++

---
+++ Source/VRAndreMalraux/Private/GrabObject.cpp
@@ -0,0 +1,30 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "GrabObject.h"
+
+// Sets default values
+AGrabObject::AGrabObject()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	// Construct, add a CustomMesh called "Mesh" init proprety
+	Mesh = CreateDefaultSubobject<UStaticMeshComponent>("Mesh");
+	Mesh->SetSimulatePhysics(true);
+}
+
+// Called when the game starts or when spawned
+void AGrabObject::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AGrabObject::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
