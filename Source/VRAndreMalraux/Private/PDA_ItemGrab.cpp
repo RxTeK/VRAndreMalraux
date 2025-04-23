e086cdcb6214e40fde945896263c5d1e4e8cc657
Morgan50 <115717766+Morgan5000001@users.noreply.github.com>
2025-04-23 11:53:12 +0200

    Roi du push

---
+++ Source/VRAndreMalraux/Private/PDA_ItemGrab.cpp
@@ -0,0 +1,25 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "PDA_ItemGrab.h"
+
+UPDA_ItemGrab::UPDA_ItemGrab()
+{
+}
+
+TSubclassOf<class AActor> UPDA_ItemGrab::GetActorClass() const
+{
+	return ActorClass;
+}
+
+UTexture2D* UPDA_ItemGrab::GetBackTexture() const
+{
+	return BackTexture;
+}
+
+UTexture2D* UPDA_ItemGrab::GetFrontTexture() const
+{
+	return FrontTexture;
+}
+
+
---
+++ Source/VRAndreMalraux/Public/PDA_ItemGrab.h
@@ -0,0 +1,40 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "PDA_ItemGrab.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class VRANDREMALRAUX_API UPDA_ItemGrab : public UPrimaryDataAsset
+{
+	GENERATED_BODY()
+
+public:
+	//Construct de l'item
+	UPDA_ItemGrab();
+
+private:
+	UPROPERTY(EditDefaultsOnly, Category = Class)
+	TSubclassOf<class AActor> ActorClass;
+
+	UPROPERTY(EditDefaultsOnly)
+	UTexture2D* FrontTexture;
+
+	UPROPERTY(EditDefaultsOnly)
+	UTexture2D* BackTexture;
+
+public:
+	UFUNCTION(BlueprintPure, Category = Class)
+	TSubclassOf<class AActor> GetActorClass() const;
+
+	UFUNCTION(BlueprintPure)
+	UTexture2D* GetFrontTexture() const;
+
+	UFUNCTION(BlueprintPure)
+	UTexture2D* GetBackTexture() const;
+};
